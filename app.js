651ab79aa02bcae3f7ac66092a3f3fa3d3b713bb
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-02-20 10:19:29 +0000

    format files using Prettier

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const {
   getTopics,
   getEndpoints,
   getArticleById,
   handleNonExist,
-  getArticles
-} = require('./controllers/controllers')
+  getArticles,
+} = require("./controllers/controllers");
 
-app.get('/api/topics', getTopics)
+app.get("/api/topics", getTopics);
 
-app.get('/api', getEndpoints)
+app.get("/api", getEndpoints);
 
-app.get('/api/articles/:article_id', getArticleById)
+app.get("/api/articles/:article_id", getArticleById);
 
-app.get('/api/articles', getArticles)
+app.get("/api/articles", getArticles);
 
-app.all('/*', handleNonExist)
+app.all("/*", handleNonExist);
 
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg })
+    res.status(err.status).send({ msg: err.msg });
   }
 
-  if (err.code === '22P02') {
-    res.status(400).send({ msg: 'Bad request' })
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
   }
-})
+});
 
-module.exports = { app }
+module.exports = { app };
